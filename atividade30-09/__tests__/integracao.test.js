1d671505bc1d8a96a023c2f575efce210bc57552
BÁRBARA COELHO RIOS <barbara_rios@estudante.sesisenai.org.br>
2024-10-07 19:37:19 -0300

    aaa

--- atividade30-09/__tests__/integracao.test.js
+++ atividade30-09/__tests__/integracao.test.js
@@ -1,32 +1,59 @@
-//importar nossos modulos
-const { addUser, getUser, deleteUser } = require('../src/users')
-const { addOrder, getOrder, deleteOrder } = require('../src/orders')
+// Importar nossos módulos
+const { addUser, getUser, deleteUser } = require('../src/users');
+const { addOrder, getOrder, deleteOrder } = require('../src/orders');
 
-//estrutura 
-//testando funcao adicionar 
-describe('Testando a funcao adicionar pedidos e usuarios', () => {
-    test('adicionar pedidos e usuarios', () => {
-        const user = {id: 1, 
-                    name: "João"
-                }
+// Estrutura
+// Testando função adicionar
+describe('Testando a função adicionar pedidos e usuários', () => {
+    
+    test('Adicionar pedidos e usuários', () => {
+        const user = {
+            id: 1,
+            name: "João"
+        };
         const order = {
             id: 100,
             item: "mouse",
             userId: 1
-        }
+        };
 
-        addUser(user)
-        addOrder(order)
+        addUser(user);
+        addOrder(order);
 
-        const findUser = getUser(1)
-        const findOrder = getOrder(100)
+        const findUser = getUser(1);
+        const findOrder = getOrder(100);
 
-        expect(findUser).toEqual(user)
-        expect(findOrder).toEqual(order)
-        //verificar a integracao dos dois
-        expect(findOrder.userId).toBe(findUser.id)
-    })
+        expect(findUser).toEqual(user);
+        expect(findOrder).toEqual(order);
+        // Verificar a integração dos dois
+        expect(findOrder.userId).toBe(findUser.id);
+    });
 
-    
+    test('Testando a função excluir', () => {
+        // Adicionar usuário e pedido
+        const user = {
+            id: 2,
+            name: 'Maria'
+        };
+        const order = {
+            id: 200,
+            item: 'Cadeira',
+            userId: 2
+        };
 
-})
+        addUser(user);
+        addOrder(order);
+
+        // Deletar primeiro o pedido, pois está vinculado ao usuário
+        deleteOrder(200);
+        deleteUser(2);
+
+        // Buscando usuário e pedido na lista
+        const findUser = getUser(2);
+        const findOrder = getOrder(200);
+
+        // Verificando se o resultado da busca dá undefined
+        expect(findUser).toBeUndefined();
+        expect(findOrder).toBeUndefined();
+    });
+});
